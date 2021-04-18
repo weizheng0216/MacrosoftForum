867bbc73ac2715b533e169719e88a2590edb661a
Haocheng Gao <hag223@lehigh.edu>
2021-04-19 00:28:20 +0800

    Remove outdated comment

--- backend/src/main/java/edu/lehigh/cse216/macrosoft/backend/json/VoteRequest.java
+++ backend/src/main/java/edu/lehigh/cse216/macrosoft/backend/json/VoteRequest.java
@@ -1,7 +1,6 @@
 package edu.lehigh.cse216.macrosoft.backend.json;
 
 public class VoteRequest implements ValidateFormat {
-    // use int to prevent json error
     public boolean upVote;
     public boolean downVote;
 
